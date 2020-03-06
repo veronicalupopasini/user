7482c76296550b1561181b53111adcfb07e2a545
Veronica Lupo Pasini <v.lupopasini@esconsulting.it>
2020-03-06 17:24:10 +0100

    Removed unnecessary methods from EscUserRepository

--- src/Repository/EscUserRepository.php
+++ src/Repository/EscUserRepository.php
@@ -2,7 +2,6 @@
 
 namespace Esc\User\Repository;
 
-use Doctrine\Common\Collections\Criteria;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
 
 interface EscUserRepository
@@ -24,44 +23,6 @@ interface EscUserRepository
      * @return mixed
      */
     public function readOneById(int $id);
-
-    /**
-     * @param AttributeBag $parameters
-     * @return Criteria
-     */
-    public function getPaginatedAndFilteredCriteria(AttributeBag $parameters): Criteria;
-
-    /***
-     * @param array $filters
-     * @return Criteria
-     */
-    public function getFiltersCriteria(array $filters): Criteria;
-
-    /**
-     * @param Criteria $criteria
-     * @param string $username
-     * @return Criteria
-     */
-    public function getUsernameCriteria(Criteria $criteria, string $username): Criteria;
-
-    /**
-     * @param Criteria $criteria
-     * @param string $active
-     * @return Criteria
-     */
-    public function getActiveCriteria(Criteria $criteria, string $active): Criteria;
-
-    /**
-     * @param array $filters
-     * @return AttributeBag
-     */
-    public function prepareFiltersCriteria(array $filters): AttributeBag;
-
-    /**
-     * @param int $id
-     * @return mixed
-     */
-    public function findOneById(int $id);
 
     /**
      * @param int $id
